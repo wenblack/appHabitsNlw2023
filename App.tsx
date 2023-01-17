d69b5701951d0fcdc88388c2d579fbd0322a9e99
Wender Barbosa <wenblack012@gmail.com>
2023-01-17 00:19:20 -0300

    Create Loading Screen and add custom fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import { Loading } from './src/components/Loading'
+import {
+	useFonts,
+	Inter_400Regular,
+	Inter_600SemiBold,
+	Inter_700Bold,
+	Inter_800ExtraBold
+} from '@expo-google-fonts/inter'
 export default function App() {
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_600SemiBold,
+		Inter_700Bold,
+		Inter_800ExtraBold
+	})
+
+	if (!fontsLoaded) {
+		return <Loading></Loading>
+	}
+
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style='auto' />
+			<Text style={styles.text}>
+				Open up App.tsx to start working on your app!
+			</Text>
+			<StatusBar
+				barStyle={'light-content'}
+				backgroundColor='transparent'
+				translucent
+			/>
 		</View>
 	)
 }
@@ -13,8 +36,12 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: '#09090A',
 		alignItems: 'center',
 		justifyContent: 'center'
+	},
+	text: {
+		color: '#ffff',
+		fontFamily: 'Inter_600SemiBold'
 	}
 })
